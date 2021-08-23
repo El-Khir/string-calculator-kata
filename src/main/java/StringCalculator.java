12019af3668148fa83ab4d575c27e47ca0e67d43
zouaoui elkhir <elkhirzouaoui@gmail.com>
2021-08-23 21:00:32 +0200

    step 5: throw exception if contains negative numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -7,12 +7,16 @@ public class StringCalculator {
     public static final String DEFAULT_NUMBERS_DELIMITER_REGEX = "[,\n]";
     public static final String REGEX_CHARACTER_PROTECTION = "\\";
     public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String NEGATIVES_NOT_ALLOWED_EXCEPTION_PREFIX_MESSAGE = "negatives not allowed : ";
+    public static final String NUMBERS_IN_MESSAGE_DELIMITER = ", ";
 
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-        return sum(toIntArray(numbers));
+        int[] numbersAsIntArray = toIntArray(numbers);
+        throwExceptionIfContainsNegatives(numbersAsIntArray);
+        return sum(numbersAsIntArray);
     }
 
     private int[] toIntArray(String numbers) {
@@ -31,6 +35,23 @@ public class StringCalculator {
         return DEFAULT_NUMBERS_DELIMITER_REGEX;
     }
 
+    private void throwExceptionIfContainsNegatives(int[] numbers) {
+        int[] negativeNumbers = Arrays.stream(numbers)
+                .filter(number -> number < 0)
+                .toArray();
+        if (negativeNumbers.length > 0) {
+            throw new IllegalArgumentException(buildNegativeNotAllowedExceptionMessage(negativeNumbers));
+        }
+    }
+
+    private String buildNegativeNotAllowedExceptionMessage(int[] negativeNumbers) {
+        String commaSeparatedNegativeNumbers = Arrays.stream(negativeNumbers)
+                .mapToObj(String::valueOf)
+                .collect(joining(NUMBERS_IN_MESSAGE_DELIMITER));
+        return NEGATIVES_NOT_ALLOWED_EXCEPTION_PREFIX_MESSAGE +
+                commaSeparatedNegativeNumbers;
+    }
+
     private String removeCustomDelimiterLine(String numbers) {
         if (containsCustomDelimiter(numbers)) {
             return numbers.lines().skip(1).collect(joining());
@@ -38,11 +59,11 @@ public class StringCalculator {
         return numbers;
     }
 
-    private int sum(int[] numbers) {
-        return Arrays.stream(numbers).sum();
-    }
-
     private boolean containsCustomDelimiter(String numbers) {
         return numbers.startsWith(CUSTOM_DELIMITER_PREFIX);
+    }
+
+    private int sum(int[] numbers) {
+        return Arrays.stream(numbers).sum();
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class StringCalculatorTest {
@@ -14,28 +15,43 @@ public class StringCalculatorTest {
 
     @Test
     public void givenStringContainingOnly4_whenAdd_thenReturn4() {
-        assertEquals(stringCalculator.add("4"), 4);
+        assertEquals(4, stringCalculator.add("4"));
     }
 
     @Test
     public void givenStringContaining6And4_whenAdd_thenReturn10() {
-        assertEquals(stringCalculator.add("6,4"), 10);
+        assertEquals(10, stringCalculator.add("6,4"));
     }
 
     @Test
     public void givenStringContainingFromZeroTo9_whenAdd_thenReturn45() {
-        assertEquals(stringCalculator.add("0,1,2,3,4,5,6,7,8,9"), 45);
+        assertEquals(45, stringCalculator.add("0,1,2,3,4,5,6,7,8,9"));
     }
 
     @Test
     public void givenStringContainingFrom1To3AndNewLineDelimiter_whenAdd_thenReturn6() {
-        assertEquals(stringCalculator.add("1\n2,3"), 6);
+        assertEquals(6, stringCalculator.add("1\n2,3"));
     }
 
     @Test
-    public void givenStringContainingFrom1And2_withSemicolonDelimiter_whenAdd_thenReturn3() {
-        assertEquals(stringCalculator.add("//;\n1;2"), 3);
+    public void givenStringContaining1And2_withSemicolonDelimiter_whenAdd_thenReturn3() {
+        assertEquals(3, stringCalculator.add("//;\n1;2"));
     }
 
+    @Test
+    public void givenStringContainingNegatives_whenAdd_thenIllegalArgumentExceptionThrownWithAllNegativesInMessage() {
+        IllegalArgumentException thrownException = assertThrows(IllegalArgumentException.class,
+                () -> stringCalculator.add("1,-6,10,0,-2,11\n-100")
+        );
+        assertEquals("negatives not allowed : -6, -2, -100", thrownException.getMessage());
+    }
+
+    @Test
+    public void givenStringContainingNegatives_withSemicolonDelimiter_whenAdd_thenIllegalArgumentExceptionThrownWithAllNegativesInMessage() {
+        IllegalArgumentException thrownException = assertThrows(IllegalArgumentException.class,
+                () -> stringCalculator.add("//;\n-1;-6;-11")
+        );
+        assertEquals("negatives not allowed : -1, -6, -11", thrownException.getMessage());
+    }
 
 }
